19c1c3a564315602090020049c5bddb7d818cecb
Ondřej Čertík <ondrej@certik.us>
2026-04-07 23:06:46 -0600

    Download de440s.bsp

--- download_data.sh
+++ download_data.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 URL=https://github.com/certik/ephemeris/releases/download/v0.1.0
+curl -fSLO --progress-bar "$URL/de440s.bsp"
 curl -fSLO --progress-bar "$URL/de441s.bsp"
 curl -fSLO --progress-bar "$URL/latest_eop2.long"
 curl -fSLO --progress-bar "$URL/nutation.dat"
 shasum -a 256 -c <<EOF
+c1c7feeab882263fc493a9d5a5b2ddd71b54826cdf65d8d17a76126b260a49f2  de440s.bsp
 e51e348e42dfc68d58b0aafb656836515e890cc1410931473d0cc6ebae0f140e  de441s.bsp
 a0271731924132875456e6e714360f786bf0a261e73fe39c8e9d463934ad5881  latest_eop2.long
 f7d8f4d701ef7d0887511c7014b0b5db2fbea1d558d0b6a6e29d57469a6c2d2f  nutation.dat
